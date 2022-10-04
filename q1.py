57cc93c246ea24ffbc6b3c4a8edcfd2fa8616328
ST11ZY <57012943+ST11ZY@users.noreply.github.com>
2022-10-04 12:02:31 -0700

    Update q1.py

--- q1.py
+++ q1.py
@@ -2,44 +2,40 @@
 # ******************************
 # Make your Code
 # ******************************
-x=[]
-y=10
+list=[]
+totalnums=10
 i=0
 counter=0
 sum=0
-z=0
-while y>0:
+firstcount=0
+while totalnums>0:
     inp=input("Value?")
     num=int(inp)
-    x.append(num)
-    y=y-1
-while z==0:
+    list.append(num)
+    totalnums=totalnums-1
+while firstcount==0:
   if i==9:
     break
-  if x[i]%2==0 and x[i+1]%2==0:
+  if list[i]%2==0 and list[i+1]%2==0:
     counter=counter+1
     i= i+1
-    z=z+1
+    firstcount=firstcount+1
   else:
     i=i+1
-while z>0:
+while firstcount>0:
   sum=1
   while sum%2==1:
     i=i+1
     if i>7:
       break
-    if x[i]%2==0:
+    if list[i]%2==0:
       continue;
-    if x[i]%2==1 and x[i+1]%2==1:
-      continue;
-    if x[i]%2==0 and x[i+1]%2==0 and x[i+2]%2==0:
-      continue
-    else:
+    if list[i]%2==1 and list[i+1]%2==0 and list[i+2]%2==0:
       counter=counter+1
-      continue
+      continue;
   if i>7:
     break    
-print(counter)    
+print(counter)       
 
 
 # Requirement
